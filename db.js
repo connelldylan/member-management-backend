4d89de473e089c95edd8cdd0a20d9260c941a169
Dylan Connell <dconnell3599@gmail.com>
2025-03-11 13:49:15 -0500

    Added longer connection

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }, // Ensure SSL is enabled
+    connectionTimeoutMillis: 10000, // Increase timeout to 10s
+    idleTimeoutMillis: 30000 // Keep connection open longer
 });
 
 module.exports = pool;
+
