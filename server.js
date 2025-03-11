2591dcd4bd0beb5409a231956e62d4c92c87395c
Dylan Connell <dconnell3599@gmail.com>
2025-03-10 21:52:15 -0500

    debugging error issue with rendering backend

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
